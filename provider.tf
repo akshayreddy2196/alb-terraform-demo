017b6989ba27477fe38473dae144310edcdf6a53
akshayreddy2196 <77155965+akshayreddy2196@users.noreply.github.com>
2025-09-04 22:25:15 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,15 +1,1 @@
-#terraform {
-##    source  = "hashicorp/aws"
- #     version = "~> 5.0"
-  #  }
-  #}
-  #backend "s3" {
-  #  bucket         = "devops-tfstate-hu1"
-   # key            = "alb-nginx/terraform.tfstate"
-    #region         = "us-east-1"
-    #encrypt        = true
-  #}
-#}
 
-## region = "us-east-1"
-#}
