f6f316c503b4b69bb7fa351c2588287d38214fd8
akshayreddy2196 <77155965+akshayreddy2196@users.noreply.github.com>
2025-09-04 22:09:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "devops-tfstate-hu1"
+    key            = "alb-nginx/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 # --- VPC ---
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
