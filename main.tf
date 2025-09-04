c368aad569990f4faa9d80035de0b61d570e9550
akshayreddy2196 <77155965+akshayreddy2196@users.noreply.github.com>
2025-09-04 21:56:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,9 @@ resource "aws_subnet" "public_subnet1" {
   cidr_block              = var.public_subnet_cidrs[0]
   availability_zone       = var.availability_zones[0]
   map_public_ip_on_launch = true
-  tags = { Name = "alb-public-subnet1" }
+  tags = {
+    Name = "alb-public-subnet1"
+  }
 }
 
 resource "aws_subnet" "public_subnet2" {
@@ -28,7 +30,9 @@ resource "aws_subnet" "public_subnet2" {
   cidr_block              = var.public_subnet_cidrs[1]
   availability_zone       = var.availability_zones[1]
   map_public_ip_on_launch = true
-  tags = { Name = "alb-public-subnet2" }
+  tags = {
+    Name = "alb-public-subnet2"
+  }
 }
 
 resource "aws_subnet" "public_subnet3" {
@@ -36,17 +40,23 @@ resource "aws_subnet" "public_subnet3" {
   cidr_block              = var.public_subnet_cidrs[2]
   availability_zone       = var.availability_zones[2]
   map_public_ip_on_launch = true
-  tags = { Name = "alb-public-subnet3" }
+  tags = {
+    Name = "alb-public-subnet3"
+  }
 }
 
 # --- Route Table ---
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
+
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main.id
   }
-  tags = { Name = "alb-public-rt" }
+
+  tags = {
+    Name = "alb-public-rt"
+  }
 }
 
 resource "aws_route_table_association" "public_associate1" {
@@ -101,83 +111,94 @@ resource "aws_security_group" "alb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = { Name = "alb-sg" }
+  tags = {
+    Name = "alb-sg"
+  }
 }
 
 # --- EC2 Instances ---
 resource "aws_instance" "web_server_1" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.public_subnet1.id
-  vpc_security_group_ids = [aws_security_group.alb_sg.id]
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public_subnet1.id
+  vpc_security_group_ids      = [aws_security_group.alb_sg.id]
   associate_public_ip_address = true
 
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo dnf update -y
-              sudo dnf install -y nginx.x86_64
-              sudo systemctl start nginx
-              sudo systemctl enable nginx
-              echo "<h1>${var.page_titles[0]}</h1>" | sudo tee -a /usr/share/nginx/html/index.html
-              sudo chown -R nginx:nginx /usr/share/nginx/html
-              sudo chmod -R 755 /usr/share/nginx/html
-              EOF
+  user_data = <<EOF
+   #!/bin/bash
+   sudo dnf update -y
+   sudo dnf install -y nginx.x86_64
+   sudo systemctl start nginx
+   sudo systemctl enable nginx
+   echo "<h1>${var.page_titles[0]}</h1>" | sudo tee -a /usr/share/nginx/html/index.html
+   sudo chown -R nginx:nginx /usr/share/nginx/html
+   sudo chmod -R 755 /usr/share/nginx/html
+   EOF
 
-  tags = { Name = "WebServer-sub-1" }
+  tags = {
+    Name = "WebServer-sub-1"
+  }
 }
 
 resource "aws_instance" "web_server_2" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.public_subnet2.id
-  vpc_security_group_ids = [aws_security_group.alb_sg.id]
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public_subnet2.id
+  vpc_security_group_ids      = [aws_security_group.alb_sg.id]
   associate_public_ip_address = true
 
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo dnf update -y
-              sudo dnf install -y nginx.x86_64
-              sudo systemctl start nginx
-              sudo systemctl enable nginx
-              mkdir /usr/share/nginx/html/images
-              echo "<h1>${var.page_titles[1]}</h1>" | sudo tee -a /usr/share/nginx/html/images/index.html
-              sudo chown -R nginx:nginx /usr/share/nginx/html/images
-              sudo chmod -R 755 /usr/share/nginx/html/images
-              EOF
+  user_data = <<EOF
+   #!/bin/bash
+   sudo dnf update -y
+   sudo dnf install -y nginx.x86_64
+   sudo systemctl start nginx
+   sudo systemctl enable nginx
+   mkdir /usr/share/nginx/html/images
+   echo "<h1>${var.page_titles[1]}</h1>" | sudo tee -a /usr/share/nginx/html/images/index.html
+   sudo chown -R nginx:nginx /usr/share/nginx/html/images
+   sudo chmod -R 755 /usr/share/nginx/html/images
+   EOF
 
-  tags = { Name = "WebServer-sub-2" }
+  tags = {
+    Name = "WebServer-sub-2"
+  }
 }
 
 resource "aws_instance" "web_server_3" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.public_subnet3.id
-  vpc_security_group_ids = [aws_security_group.alb_sg.id]
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public_subnet3.id
+  vpc_security_group_ids      = [aws_security_group.alb_sg.id]
   associate_public_ip_address = true
 
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo dnf update -y
-              sudo dnf install -y nginx.x86_64
-              sudo systemctl start nginx
-              sudo systemctl enable nginx
-              mkdir /usr/share/nginx/html/register
-              echo "<h1>${var.page_titles[2]}</h1>" | sudo tee -a /usr/share/nginx/html/register/index.html
-              sudo chown -R nginx:nginx /usr/share/nginx/html/register
-              sudo chmod -R 755 /usr/share/nginx/html/register
-              EOF
+  user_data = <<EOF
+   #!/bin/bash
+   sudo dnf update -y
+   sudo dnf install -y nginx.x86_64
+   sudo systemctl start nginx
+   sudo systemctl enable nginx
+   mkdir /usr/share/nginx/html/register
+   echo "<h1>${var.page_titles[2]}</h1>" | sudo tee -a /usr/share/nginx/html/register/index.html
+   sudo chown -R nginx:nginx /usr/share/nginx/html/register
+   sudo chmod -R 755 /usr/share/nginx/html/register
+   EOF
 
-  tags = { Name = "WebServer-sub-3" }
+  tags = {
+    Name = "WebServer-sub-3"
+  }
 }
 
 # --- ALB ---
 resource "aws_lb" "alb" {
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = [aws_subnet.public_subnet1.id, aws_subnet.public_subnet2.id, aws_subnet.public_subnet3.id]
-  enable_deletion_protection = false
-  tags = { Name = "path-alb" }
+  internal                   = false
+  load_balancer_type          = "application"
+  security_groups             = [aws_security_group.alb_sg.id]
+  subnets                     = [aws_subnet.public_subnet1.id, aws_subnet.public_subnet2.id, aws_subnet.public_subnet3.id]
+  enable_deletion_protection  = false
+
+  tags = {
+    Name = "path-alb"
+  }
 }
 
 # --- Target Groups ---
@@ -197,7 +218,10 @@ resource "aws_lb_target_group" "alb_tg_home" {
     healthy_threshold   = 2
     unhealthy_threshold = 5
   }
-  tags = { Name = "alb-tg-home" }
+
+  tags = {
+    Name = "alb-tg-home"
+  }
 }
 
 resource "aws_lb_target_group" "alb_tg_images" {
@@ -216,7 +240,10 @@ resource "aws_lb_target_group" "alb_tg_images" {
     healthy_threshold   = 2
     unhealthy_threshold = 5
   }
-  tags = { Name = "alb-tg-images" }
+
+  tags = {
+    Name = "alb-tg-images"
+  }
 }
 
 resource "aws_lb_target_group" "alb_tg_register" {
@@ -235,7 +262,10 @@ resource "aws_lb_target_group" "alb_tg_register" {
     healthy_threshold   = 2
     unhealthy_threshold = 5
   }
-  tags = { Name = "alb-tg-register" }
+
+  tags = {
+    Name = "alb-tg-register"
+  }
 }
 
 # --- ALB Listener ---
@@ -254,10 +284,12 @@ resource "aws_lb_listener" "alb_http_listener" {
 resource "aws_lb_listener_rule" "alb_rule_images" {
   listener_arn = aws_lb_listener.alb_http_listener.arn
   priority     = 10
+
   action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.alb_tg_images.arn
   }
+
   condition {
     path_pattern {
       values = ["/images/*"]
@@ -268,10 +300,12 @@ resource "aws_lb_listener_rule" "alb_rule_images" {
 resource "aws_lb_listener_rule" "alb_rule_register" {
   listener_arn = aws_lb_listener.alb_http_listener.arn
   priority     = 20
+
   action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.alb_tg_register.arn
   }
+
   condition {
     path_pattern {
       values = ["/register/*"]
@@ -292,5 +326,5 @@ resource "aws_lb_target_group_attachment" "alb_tg_attach_images" {
 
 resource "aws_lb_target_group_attachment" "alb_tg_attach_register" {
   target_group_arn = aws_lb_target_group.alb_tg_register.arn
-  target_id        = aws_instance.web_server_3.id 
+  target_id        = aws_instance.web_server_3.id
 }
