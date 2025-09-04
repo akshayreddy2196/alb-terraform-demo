8262081578422206c075dab11275cb827a4f36f7
akshayreddy2196 <77155965+akshayreddy2196@users.noreply.github.com>
2025-09-04 22:10:48 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,18 +1,15 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-  backend "s3" {
-    bucket         = "devops-tfstate-hu1"
-    key            = "alb-nginx/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-  }
-}
+#terraform {
+##    source  = "hashicorp/aws"
+ #     version = "~> 5.0"
+  #  }
+  #}
+  #backend "s3" {
+  #  bucket         = "devops-tfstate-hu1"
+   # key            = "alb-nginx/terraform.tfstate"
+    #region         = "us-east-1"
+    #encrypt        = true
+  #}
+#}
 
-provider "aws" {
-  region = "us-east-1"
-}
+## region = "us-east-1"
+#}
